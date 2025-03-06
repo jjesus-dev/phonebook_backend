ef63a7ceacb9ecc18b0a0155c32dd5b09f80adb8
Javier J. Martínez M. <jj_dev@outlook.com>
2025-03-05 19:25:59 -0600

    feat: add error handling middleware for exercise 3.16
    add error handling middleware to phonebook backend

--- index.js
+++ index.js
@@ -21,6 +21,16 @@ morgan.token('body', (req, res) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
+const errorHandler = (error, request, response, next) => {
+    console.error('ERROR:', error.message);
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformed id' });
+    } 
+    
+    next(error);
+}
+
 app.get('/', (request, response) => {
     response.send('<p>Phone numbers</p>');
 })
@@ -39,21 +49,19 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(number => {
-        response.json(number);
-    })
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(number => {
+            response.json(number);
+        }).catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndDelete(request.params.id)
         .then(result => {
             response.status(204).end();
         })
-        .catch(error => {
-            console.error(error);
-            response.status(400).send({ error: 'malformatted id' });
-        })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -78,6 +86,8 @@ app.post('/api/persons', (request, response) => {
         response.json(savedNumber);
     })
 })
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
