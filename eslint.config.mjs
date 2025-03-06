d070e9e67b0871cc68d73427d216b26125897135
Javier J. Martínez M. <jj_dev@outlook.com>
2025-03-06 14:17:19 -0600

    feat: add linting packages & configuration file

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import globals from "globals";
+import stylisticJs from '@stylistic/eslint-plugin-js';
+import js from "@eslint/js";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"},
+    plugins: {
+      '@stylistic/js': stylisticJs
+    },
+    rules: {
+      'eqeqeq': 'error',
+      'no-console': "off",
+      '@stylistic/js/indent': ["error", 2],
+      '@stylistic/js/linebreak-style': ["error", "unix"],
+      '@stylistic/js/quotes': ["error", "single"],
+      '@stylistic/js/semi': ["error", "always", { "omitLastInOneLineClassBody": true}],
+      '@stylistic/js/arrow-spacing': "error",
+      '@stylistic/js/object-curly-spacing': ["error", "always"],
+      '@stylistic/js/no-trailing-spaces': "error",
+    },
+  },// ignore a dir globally
+  {ignores: ["dist/"]},
+  js.configs.recommended, {},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+];
